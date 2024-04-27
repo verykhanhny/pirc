33f686a915e6120888ba8dbbacf868e6f6bae695
Khanh Duong <verykhanhny@gmail.com>
2024-04-27 00:27:02 -0600

    authenticate controller

--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,72 @@
-const WebSocket = require('ws');
+const https = require('https');
+const websocket = require('ws');
+require('dotenv').config();
+
+// Define login credentials
+const loginData = JSON.stringify({
+  username: process.env.username,
+  password: process.env.password
+});
+
+// Prepare request options
+const options = {
+  hostname: 'internal.khanhduong.dev',
+  port: 61386,
+  path: '/login',
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': loginData.length
+  }
+};
+
+// Login cookie
+let cookie = [];
 
 // Send data in chunks every second
-let intervalId = 0
+let intervalId = 0;
+
+function login() {
+  const req = https.request(options, res => {
+    let data = '';
+
+    // A chunk of data has been received.
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      if (res.statusCode === 200) {
+        // Save received session cookie in the client
+        console.log('Login successful!');
+        cookie = res.headers['set-cookie'];
+        connect()
+      } else {
+        console.log("Login failed:", data)
+        setTimeout(login, 3000)
+      }
+    });
+  });
+
+  // Handle error
+  req.on('error', (error) => {
+    console.log('Error:', error);
+  });
+
+  // Send login data
+  req.write(loginData);
+  req.end();
+}
+
+login()
 
 function connect() {
   console.log('Connecting to WebSocket...');
-  const ws = new WebSocket('wss://internal.khanhduong.dev:61386');
+  const ws = new websocket('wss://internal.khanhduong.dev:61386', {
+    headers: {
+      cookie: cookie
+    }
+  });
 
   ws.onopen = function () {
     console.log('WebSocket connection opened');
@@ -15,17 +76,22 @@ function connect() {
   };
 
   ws.onmessage = function (event) {
-    console.log('Received: %s', JSON.parse(event.data))
+    const message = JSON.parse(event.data);
+    console.log('Received: %s', message);
+    // Got error so try to login again
+    if (message.code) {
+      ws.close();
+    }
   };
 
   ws.onerror = function(error) {
-    console.log('Websocket error: %s', error.message)
+    console.log('Websocket error: %s', error.message);
   }
 
   ws.onclose = function () {
     console.log('WebSocket connection closed');
-    clearInterval(intervalId)
-    setTimeout(connect, 3000)
+    clearInterval(intervalId);
+    setTimeout(login, 3000);
   }
 }
 
@@ -33,5 +99,3 @@ function connect() {
 function generateData() {
   return Math.random().toString();
 }
-
-connect()
